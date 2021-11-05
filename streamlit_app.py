0f7ecbc221f6b489b69ffbcf1c7c223c2e0baa91
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2021-11-05 18:22:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,16 @@
 import streamlit as st
 import pandas as pd
+from streamlit.logger import get_logger
+import numpy as np 
+import os
+import matplotlib.pyplot as plt
 
 
-st.write("First App")
-#df = pd.read_csv("titanic-data.csv")
-#st.line_chart(df)
+logger = get_logger(__name__)
+
+
+st.sidebar.header("Paramètres")
+st.title("La Coupe Du Monde De Football")
+st.header("Description")
+st.write("Il s’agit de représenter pour la coupe du monde de football (2014, ..., 1954) le meilleur buteur et son pays d’origine ayant le record des buts inscrits par le même joueur en indiquant le nombre de buts inscrits et les éditions dans laquelle il les a inscrit. Les données sont dans le fichier csv suivant.Donnez la représentation visuelle adéquate.")
+         
