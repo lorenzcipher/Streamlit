27a70823228d6c1bd5fb5013cadd0339fc3ee364
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2021-11-06 16:48:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,24 +19,9 @@ st.write("Il s’agit de représenter pour la coupe du monde de football (2014, 
 Annee = st.sidebar.slider("Année", 1954, 2014, 1954, 4)
 raking_pays = st.sidebar.selectbox('Ranking par pays ', ('Tous les pays','Germany', 'Brazil','France','Hungray','UK','Argentina','Peru','Poland'))
 
-DATA_URL = 'covid.csv'
-def load_data():
-    data = pd.read_csv(DATA_URL)
-    data['Date'] = pd.to_datetime(data['Date']).dt.strftime('%Y-%m-%d')
-    return data
-df = load_data()
 
-# show data on streamlit
-st.write(df)
 
-# Filters UI
-subset_data = df
-country_name_input = st.sidebar.multiselect(
-'Country name',
-df.groupby('Country/Region').count().reset_index()['Country/Region'].tolist())
-# by country name
-if len(country_name_input) > 0:
-    subset_data = df[df['Country/Region'].isin(country_name_input)]
+
 metrics =['total_cases','new_cases','total_deaths','new_deaths','total_cases_per_million','new_cases_per_million','total_deaths_per_million','new_deaths_per_million','total_tests','new_tests','total_tests_per_thousand','new_tests_per_thousand']
 cols = st.selectbox('Covid metric to view', metrics)
 # let's ask the user which column should be used as Index
