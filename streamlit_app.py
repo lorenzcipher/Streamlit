14f361197aec425f1b6b39a8f25568760833241d
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2021-11-06 17:08:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ st.header("Description")
 st.write("Il s’agit de représenter pour la coupe du monde de football (2014, ..., 1954) le meilleur buteur et son pays d’origine ayant le record des buts inscrits par le même joueur en indiquant le nombre de buts inscrits et les éditions dans laquelle il les a inscrit. Les données sont dans le fichier csv suivant.Donnez la représentation visuelle adéquate.")
 Annee = st.sidebar.slider("Année", 1954, 2014, 1954, 4)
 
+
 pays = ['Tous les pays','Germany', 'Brazil','France','Hungray','UK','Argentina','Peru','Poland']
 ranking_pays = st.sidebar.selectbox('Ranking par pays ', pays)
 # let's ask the user which column should be used as Index
@@ -26,8 +27,8 @@ if ranking_pays in pays:
     choix_pays = ranking_pays
 
     
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+st.write(Annee)
+
     
     
 
