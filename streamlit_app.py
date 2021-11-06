ef09acaa63208e8c18cb8f1e49dd6581ed363832
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2021-11-06 16:58:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,13 +17,14 @@ st.title("La Coupe Du Monde De Football")
 st.header("Description")
 st.write("Il s’agit de représenter pour la coupe du monde de football (2014, ..., 1954) le meilleur buteur et son pays d’origine ayant le record des buts inscrits par le même joueur en indiquant le nombre de buts inscrits et les éditions dans laquelle il les a inscrit. Les données sont dans le fichier csv suivant.Donnez la représentation visuelle adéquate.")
 Annee = st.sidebar.slider("Année", 1954, 2014, 1954, 4)
-raking_pays = st.sidebar.selectbox('Ranking par pays ', ('Tous les pays','Germany', 'Brazil','France','Hungray','UK','Argentina','Peru','Poland'))
 
-
-
-
-metrics =['total_cases','new_cases','total_deaths','new_deaths','total_cases_per_million','new_cases_per_million','total_deaths_per_million','new_deaths_per_million','total_tests','new_tests','total_tests_per_thousand','new_tests_per_thousand']
-cols = st.selectbox('Covid metric to view', metrics)
+pays = ['Tous les pays','Germany', 'Brazil','France','Hungray','UK','Argentina','Peru','Poland']
+ranking_pays = st.sidebar.selectbox('Ranking par pays ', pays)
 # let's ask the user which column should be used as Index
-if cols in metrics:   
-    metric_to_show_in_covid_Layer = cols
+if ranking_pays in pays:   
+    choix_pays = ranking_pays
+st.write(choix_pays)
+
+
+
+
