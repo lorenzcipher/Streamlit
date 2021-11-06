2da5acc9ce3838e3624649a81667d3b25cb13e7a
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2021-11-06 17:47:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,9 @@ st.map()
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  st.write(dataframe)
-
+ 
+for row in df:
+    st.write(row)
 
     
     
