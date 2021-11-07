70855dfccf8e2496070e2a971f0a9105baf64395
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2021-11-07 02:16:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,12 +27,15 @@ ranking_pays = st.sidebar.selectbox('Ranking par pays ', pays)
 tmp = df[df.Country == ranking_pays]
 
 
-totale = list(tmp["Total"])
-del tmp["Total"]
+#totale = list(tmp["Total"])
+#del tmp["Total"]
 st.write(tmp)
 
 
-c = alt.Chart(tmp).mark_circle().encode(x='Années', y='Playes', size='Buts', color='Country', tooltip=['Années', 'Playes', 'Buts'])
+
+df = pd.DataFrame(np.random.randn(200, 3),columns=['a', 'b', 'c'])
+c = alt.Chart(df).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
 st.altair_chart(c, use_container_width=True)
 
 
