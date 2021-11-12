709648a6b0b73dd57bd50751de17aece0afc1170
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2021-11-12 16:07:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,12 @@ st.write(data[data['Age'] < 18].groupby(['Sex','Pclass']).mean())
 
 
 st.write(data)
-    
+chart_data = pd.DataFrame(
+    np.random.randn(50, 3),
+    columns=["a", "b", "c"])
+
+st.write(chart_data)
+st.bar_chart(chart_data)    
     
 
 
