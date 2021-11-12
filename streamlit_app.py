03576962ddc2e895543e7daf0cd8932a136e34a4
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2021-11-12 15:37:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
+'''
+Bibliothéques pour la manipulation des dataFrame et les chart
+'''
 import streamlit as st
 import pandas as pd 
 import numpy as np 
@@ -18,59 +21,22 @@ st.title("La Coupe Du Monde De Football")
 st.header("Description")
 st.write("Il s’agit de représenter pour la coupe du monde de football (2014, ..., 1954) le meilleur buteur et son pays d’origine ayant le record des buts inscrits par le même joueur en indiquant le nombre de buts inscrits et les éditions dans laquelle il les a inscrit. Les données sont dans le fichier csv suivant.Donnez la représentation visuelle adéquate.")
 
-#importer data
-df = pd.read_csv("data.csv", sep=",", encoding="utf-8")
-
-#list par pays 
-pays = list(set(df["Country"]))
-ranking_pays = st.sidebar.selectbox('Ranking par pays ',pays)
 
 
 
+#ouvrir un fichier csv
+data = pd.read_csv('titanic-data.csv')
 
+#filtrer la dataframe
+data = data.drop(['Name','SibSp','Parch','Ticket','Fare','Cabin','Embarked'],axis=1)
 
-#marque du filtrage
-df_mask=df['Country']==ranking_pays
+#supprimer les lignes avec des informations manquante (vide)
+data = data.dropna(axis=0)
 
-#data filter dépend le pays
-filtered_df = df[df_mask]
+#groupe by class
+st.write(data.groupby(['Sex','Pclass']).mean())
 
-#max des buteurs
-max_total = max(df['Total'])
-
-#supprimer les colonnes commune
-del filtered_df['Country']
-del filtered_df['Total']
-
-
-
-#récupérer les années à partir des columns
-Col = filtered_df.columns.values
-Col = Col[1:-1]
-
-
-l = list()
-for i in range(max_total):
-    l.append(str(i))
-
-st.write(l)    
-
-
-z = pd.DataFrame(
-    Col,
-    l)
-
-st.write(z)
-
-'''
-
-c = alt.Chart(z).mark_circle().encode(
-   x='a', y='b', size='a', color='b', tooltip=['a', 'b'])
-
-st.altair_chart(c, use_container_width=True)
-
-'''
-
+st.write(data[data['Age'] < 18].groupby(['Sex','Pclass']).mean())
 
 
 
