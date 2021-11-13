dc83e1832b93b45711eafad5c1865333aef9f0c9
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2021-11-13 18:37:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,12 @@
 Bibliothéques pour la manipulation des dataFrame et les chart
 '''
 import streamlit as st
-import pandas as pd 
-import numpy as np 
-import pydeck as pdk 
-import altair as alt 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+import csv
 
 
 
@@ -19,34 +21,26 @@ st.sidebar.header("Paramètres")
 st.sidebar.info("Veuilez Selectionnez l'année et le pays que vous voulez savoir le meilleur buteur des coordonnées selectionné")
 st.title("La Coupe Du Monde De Football")
 st.header("Description")
-st.write("Il s’agit de représenter pour la coupe du monde de football (2014, ..., 1954) le meilleur buteur et son pays d’origine ayant le record des buts inscrits par le même joueur en indiquant le nombre de buts inscrits et les éditions dans laquelle il les a inscrit. Les données sont dans le fichier csv suivant.Donnez la représentation visuelle adéquate.")
-
+st.write('''Les passagers sur le bateau Titanic sont à représenter par 3 classes (hommes, femmes,
+enfants). Sur chaque compartiment (Affaire, busines, troisième classe, membres
+d’équipage). On veut représenter les survivants et les noyés par classe (H, F, E) et par
+compartiment. Donnez la représentation visuelle adéquate.''')
 
 
 
 #ouvrir un fichier csv
 data = pd.read_csv('titanic-data.csv')
 
-#filtrer la dataframe
-data = data.drop(['Name','SibSp','Parch','Ticket','Fare','Cabin','Embarked'],axis=1)
+
 
 #supprimer les lignes avec des informations manquante (vide)
 data = data.dropna(axis=0)
 
-#groupe by class
-data_stat =data.groupby(['Sex','Pclass']).mean()
-st.write(data_stat)
-
-st.write(data[data['Age'] < 18].groupby(['Sex','Pclass']).mean())
 
 
 
 
-st.write(data)
 
-
-
-st.bar_chart(data_stat)    
     
 
 
