b8cce83b9c07f273f5a81a2efcc0caecbfa60c75
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2021-11-13 20:27:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,11 @@ st.write("Les passagers sur le bateau Titanic sont à représenter par 3 classes
 image = Image.open('titanic.jpg')
 st.image(image, caption='Titanic')
 
-st.sidebar.info('''Boite à clées :
-                       * survivant (1:oui 0:Non)
-                       *Pclass (Passenger Class): 1,2,3
-                       *Sex: Male, Female
-                       *Embarked (Port of Embarkation): C = Cherbourg, Q = Queenstown, S = Southampto
+st.sidebar.info('''Boite à clées :\n
+                       -Survivant (1:oui 0:Non)\n
+                       -Pclass (Passenger Class): 1,2,3\n
+                       -Sex: Male, Female\n
+                       -Embarked (Port of Embarkation): C = Cherbourg, Q = Queenstown, S = Southampto
                        ''')
 
 options = st.sidebar.multiselect(
@@ -53,7 +53,7 @@ def make_pivot (param1, param2):
     return slice_pivot
     #return p_chart
 
-make_pivot(option[0],option[1])
+make_pivot(options[0],options[1])
 
     
 
