377d58542d6e3016e35c7f74e33081fcd7791f5f
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2021-11-13 19:20:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import numpy as np
-#import matplotlib.pyplot as plt
 import pandas as pd
 from PIL import Image
 
@@ -20,21 +19,36 @@ st.write("Les passagers sur le bateau Titanic sont à représenter par 3 classes
 image = Image.open('titanic.jpg')
 st.image(image, caption='Titanic')
 
-st.sidebar.info("Boite à selection des analyses")
-option = st.selectbox('Selectionnez : ',('Relation entre passengers survivants et booking class', 'Relation entre passengers survivants et leurs sex', "Relation between passengers survivant et port d'embarkation"))
+st.sidebar.info('''Boite à clées :
+                       * survivant (1:oui 0:Non)
+                       *Pclass (Passenger Class): 1,2,3
+                       *Sex: Male, Female
+                       *Embarked (Port of Embarkation): C = Cherbourg, Q = Queenstown, S = Southampto
+                       ''')
+
+option = st.sidebar.selectbox('Selectionnez : ',('Relation entre passengers survivants et booking class', 'Relation entre passengers survivants et leurs sex', "Relation between passengers survivant et port d'embarkation"))
 
 
 #ouvrir un fichier csv
 data = pd.read_csv('titanic-data.csv')
 
-st.write(data)
+
 
 
 #supprimer les lignes avec des informations manquante (vide)
 data = data.dropna(axis=0)
-st.write(data)
 
-
+#fonction avec deux paramétres pour créer une dataframe et faire la relation entre les deux paramétres 
+def make_pivot (param1, param2):
+    df_slice = df[[param1, param2, 'PassengerId']]
+    slice_pivot = df_slice.pivot_table(index=[param1], columns=[param2],aggfunc=np.size, fill_value=0)
+    
+    p_chart = slice_pivot.plot.bar()
+    for p in p_chart.patches:
+        p_chart.annotate(str(p.get_height()), (p.get_x() * 1.05, p.get_height() * 1.01))
+    
+    return slice_pivot
+    return p_chart
 
 
 
